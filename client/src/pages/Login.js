dae20d1b0877cfcd33a6f9d3e36737c7d13bee0f
ritikaa-tech <ritikasinghhh27@gmail.com>
2025-05-30 00:16:09 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Login.css';
-import axios from axiosInstance;
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
